fb8e3563c4d3bd450402a9fef0c5de20ebe63d78
gumedea <gumedea@mscs-98w.mscs-ad.cs.ukzn.ac.za>
2016-04-18 11:41:28 +0200

    Initial Commit of the Bovo Board  Game

---
+++ Bovo Board Game/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * File:   main.cpp
+ * Author: gumedea
+ *
+ * Created on April 18, 2016, 11:40 AM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    //QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+cout << "Bovo" << endl;
+    //return app.exec();
+}
